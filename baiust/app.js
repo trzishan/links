9daf6209d85ac333c10ebcd7ca51195c0cfc1975
Tahmid Zishan <tahmidzishan53@gmail.com>
2026-08-08 22:46:50 +0600

    app.js added

---
+++ baiust/app.js
@@ -0,0 +1,8 @@
+// app.js
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js')
+      .then(reg => console.log('Service Worker registered!', reg))
+      .catch(err => console.error('Registration failed:', err));
+  });
+}
