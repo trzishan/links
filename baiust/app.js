cf80f1ce05b22c9adcc43dca1b312a39af61f809
Tahmid Zishan <tahmidzishan53@gmail.com>
2026-08-08 22:49:56 +0600

    maybe sw.js link fixed, idk

--- baiust/app.js
+++ baiust/app.js
@@ -1,7 +1,7 @@
 // app.js
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js')
+    navigator.serviceWorker.register('./sw.js')
       .then(reg => console.log('Service Worker registered!', reg))
       .catch(err => console.error('Registration failed:', err));
   });
